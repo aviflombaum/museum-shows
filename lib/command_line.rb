4da68e410b86e065bee41dd8b5e48667d477205a
aviflombaum <avi@flombaum.com>
2016-04-07 15:26:37 -0400

    refactored code

--- lib/command_line.rb
+++ lib/command_line.rb
@@ -1,106 +1,121 @@
-
 class CommandLine
 
-    def welcome
-      puts ""
-      puts "Retrieving museum info for you..."
-      puts ""
-      moma=Museum.new("Museum of Modern Art (MOMA)", Scraper.moma_scraper)
-      whitney=Museum.new("Whitney Museum of American Art", Scraper.whitney_scraper)
-      guggenheim=Museum.new("Guggenheim- New York", Scraper.guggenheim_scraper)
-      nat=Museum.new("American Museum of Natural History", Scraper.nat_scraper)
-      @input=nil
-      puts " ----------------------------------"
-      puts "|                                  |"
-      puts "|    Welcome to Museum Shows!!!    |"
-      puts "|                                  |"
-      puts " ----------------------------------"
-      puts ""
-      puts ""
-      call
-    end
+  def welcome
+    puts ""
+    puts "Retrieving museum info for you..."
+    puts ""
 
-    def call
-      while @input != "exit"
-        puts ""
-        puts "Here is a list of museums that we have show information for."
-        puts ""
-        Museum.print_all
-        puts ""
-        puts "'list' -            View the list of museums I can retrieve exhibit information from."
-        puts "'exit' -            Does the obvious."
-        puts ""
-        puts "Enter the number of the museum whose shows you would like to see"
-        @input=gets.strip
-        if @input.downcase == "list"
-          call
-        elsif @input.downcase == "exit"
-          exit
-        elsif @input.to_i.between?(1, Museum.all.count)
-          select_exhibit_by_museum
-        else
-          "I don't understand what you entered, try again"
-          call
-        end
-        if @input=="exit"
-          break
-        end
+    moma = Museum.new("Museum of Modern Art (MOMA)", Scraper.moma_scraper)
+    whitney = Museum.new("Whitney Museum of American Art", Scraper.whitney_scraper)
+    guggenheim = Museum.new("Guggenheim- New York", Scraper.guggenheim_scraper)
+    nat = Museum.new("American Museum of Natural History", Scraper.nat_scraper)
+
+    @input = nil
+
+    puts " ----------------------------------"
+    puts "|                                  |"
+    puts "|    Welcome to Museum Shows!!!    |"
+    puts "|                                  |"
+    puts " ----------------------------------"
+    puts ""
+    puts ""
+
+    call
+  end
+
+  def call
+    while @input != "exit"
+      puts ""
+      puts "Here is a list of museums that we have show information for."
+      puts ""
+
+      Museum.print_all
+
+      puts ""
+      puts "'list' -            View the list of museums I can retrieve exhibit information from."
+      puts "'exit' -            Does the obvious."
+      puts ""
+      puts "Enter the number of the museum whose shows you would like to see"
+
+      @input = gets.strip
+
+      if @input.downcase == "list"
+        call
+      elsif @input.downcase == "exit"
+        exit
+      elsif @input.to_i.between?(1, Museum.all.count)
+        select_exhibit_by_museum
+      else
+        "I don't understand what you entered, try again"
+        call
+      end
+
+      if @input == "exit"
+        break
       end
     end
+  end
 
+  def select_exhibit_by_museum
+    selected_museum = Museum.find(@input)
 
-    def select_exhibit_by_museum 
-        selected_museum = Museum.all[@input.to_i-1] 
-        puts ""
-        puts "#{selected_museum.name}:"
-        selected_museum.exhibits.each_with_index do |exhibit, index|
-          puts ""
-          if index.between?(0,8)
-             spacer= " "
-          else
-            spacer=""
-          end
-          puts "#{spacer}#{index+1}.   #{exhibit.title}   #{exhibit.closing_date}"
-        end
-        puts ""
-        puts "Enter the number of the show you would like to get more info about."
-        puts "Type 'back' to return to the main menu, or 'exit' to quit."
-        @input3= gets.strip
-        if @input3.downcase == "back"
-          call
-        elsif @input3.downcase == "exit"
-          @input="exit"
-          exit
+    puts ""
+    puts "#{selected_museum.name}:"
 
-        elsif @input3.to_i<1 || @input3.to_i> selected_museum.exhibits.count
-          puts "I don't understand what you entered. Please try again."
-          select_exhibit_by_museum
-        else
-          exhibit_info= selected_museum.exhibits[@input3.to_i-1]
-          puts "#{exhibit_info.title}: at #{selected_museum.name}"
-          puts ""
-          puts "#{exhibit_info.about_exhibit}"
-        end
-        if @input=="exit"
-          call
-        else
-          puts ""
-          puts "Would you like to see the list of shows from #{selected_museum.name} again? (y/n)"
-          @input5=gets.strip.downcase
-        end
-        if @input5== "y"
-          select_exhibit_by_museum
-        elsif @input5=="exit"
-          @input="exit"
-          exit
-        else 
-          call
-        end
+    selected_museum.exhibits.each_with_index do |exhibit, index|
+      puts ""
+
+      if index.between?(0,8)
+         spacer= " "
+      else
+        spacer = ""
+      end
+
+      puts "#{spacer}#{index+1}.   #{exhibit.title}   #{exhibit.closing_date}"
     end
 
-    def exit
-      puts "Thank you for using Museum Shows!"
-      puts "Goodbye."
+    puts ""
+    puts "Enter the number of the show you would like to get more info about."
+    puts "Type 'back' to return to the main menu, or 'exit' to quit."
+
+    @input3 = gets.strip
+
+    if @input3.downcase == "back"
+      call
+    elsif @input3.downcase == "exit"
+      @input = "exit"
+      exit
+    elsif @input3.to_i < 1 || @input3.to_i > selected_museum.exhibits.count
+      puts "I don't understand what you entered. Please try again."
+      select_exhibit_by_museum
+    else
+      exhibit = selected_museum.find_exhibit(@input3)
+
+      puts "#{exhibit.title}: at #{selected_museum.name}"
+      puts ""
+      puts "#{exhibit.about_exhibit}"
     end
 
+    if @input == "exit"
+      call
+    else
+      puts ""
+      puts "Would you like to see the list of shows from #{selected_museum.name} again? (y/n)"
+      @input5 = gets.strip.downcase
+    end
+
+    if @input5 == "y"
+      select_exhibit_by_museum
+    elsif @input5 == "exit"
+      @input = "exit"
+      exit
+    else
+      call
+    end
+  end
+
+  def exit
+    puts "Thank you for using Museum Shows!"
+    puts "Goodbye."
+  end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,62 +1,64 @@
-
-
 class Scraper
 
-    def self.moma_scraper
-      moma= Nokogiri::HTML(open("http://moma.org/calendar/exhibitions"))
-      moma.css("li.calendar-tile").collect do |exhibit|
-        exhibit_info={
-          :title => exhibit.css("h3.calendar-tile__title").text.strip,
-          :closing_date => exhibit.css("p").text.strip,
-          :exhibit_link => "http://moma.org#{exhibit.css("a.calendar-tile__link").attribute("href").value}"
-        }
-        page=Nokogiri::HTML(open(exhibit_info[:exhibit_link]))
-        exhibit_info[:about_exhibit]= page.css("div.mde-column__section p").text
-        
-        exhibit_info  
-      end
+  def self.moma_scraper
+    moma = Nokogiri::HTML(open("http://moma.org/calendar/exhibitions"))
+    moma.css("li.calendar-tile").collect do |exhibit|
+      exhibit_info = {
+        :title => exhibit.css("h3.calendar-tile__title").text.strip,
+        :closing_date => exhibit.css("p").text.strip,
+        :exhibit_link => "http://moma.org#{exhibit.css("a.calendar-tile__link").attribute("href").value}"
+      }
+
+      page = Nokogiri::HTML(open(exhibit_info[:exhibit_link]))
+      exhibit_info[:about_exhibit] = page.css("div.mde-column__section p").text
+
+      exhibit_info
     end
+  end
 
-    def self.whitney_scraper
-      whitney= Nokogiri::HTML(open("http://whitney.org/Exhibitions"))
-      whitney.css("div.image").collect do |exhibit|
-          exhibit_info={
-            :exhibit_link => "http://whitney.org#{exhibit.css("a").attribute("href").value}"
-             }
-          exhibit_page= Nokogiri::HTML (open(exhibit_info[:exhibit_link]))
-          exhibit_info[:title]= exhibit_page.css("div.title h1").text
-          exhibit_info[:closing_date]= exhibit_page.css("div.title h2").first.text.strip
-          exhibit_info[:about_exhibit]= exhibit_page.css("div.text-module-text p").text
-        exhibit_info
-      end
+  def self.whitney_scraper
+    whitney= Nokogiri::HTML(open("http://whitney.org/Exhibitions"))
+    whitney.css("div.image").collect do |exhibit|
+      exhibit_info= {
+        :exhibit_link => "http://whitney.org#{exhibit.css("a").attribute("href").value}"
+      }
+
+      exhibit_page = Nokogiri::HTML (open(exhibit_info[:exhibit_link]))
+      exhibit_info[:title] = exhibit_page.css("div.title h1").text
+      exhibit_info[:closing_date] = exhibit_page.css("div.title h2").first.text.strip
+      exhibit_info[:about_exhibit] = exhibit_page.css("div.text-module-text p").text
+
+      exhibit_info
     end
+  end
 
 
-    def self.guggenheim_scraper
-      guggenheim= Nokogiri::HTML(open("http://www.guggenheim.org/new-york/exhibitions/on-view/")) 
-      guggenheim.css("div.row-with-pic").collect do |exhibit|
-        exhibit_info={
-          :title => exhibit.css("div.row-text h4 a").text.strip,
-          :closing_date => exhibit.css("div.exh-dateline").text,
-          :about_exhibit => exhibit.css("div.row-text p").text
-        }
-        exhibit_info
-      end
+  def self.guggenheim_scraper
+    guggenheim = Nokogiri::HTML(open("http://www.guggenheim.org/new-york/exhibitions/on-view/"))
+    guggenheim.css("div.row-with-pic").collect do |exhibit|
+      exhibit_info = {
+        :title => exhibit.css("div.row-text h4 a").text.strip,
+        :closing_date => exhibit.css("div.exh-dateline").text,
+        :about_exhibit => exhibit.css("div.row-text p").text
+      }
+      exhibit_info
     end
+  end
 
-    def self.nat_scraper
-      exhibit_info={}
-      nat= Nokogiri::HTML(open("http://www.amnh.org/exhibitions/current-exhibitions"))
-      nat.css("section.current-exhibitions article.mod").collect do |exhibit|
-        exhibit_info={
-          :title=> exhibit.css("h1 a").text,
-          :closing_date=> exhibit.css("p.date").text,
-          :exhibit_link=> "http://www.amnh.org#{exhibit.css("h1 a").attribute("href").value}"
-        }
-        exhibit_page= Nokogiri::HTML(open(exhibit_info[:exhibit_link]))
-        exhibit_info[:about_exhibit]= exhibit_page.css("div.attribute-body p").text
-        exhibit_info
-      end
+  def self.nat_scraper
+    exhibit_info = {}
+    nat = Nokogiri::HTML(open("http://www.amnh.org/exhibitions/current-exhibitions"))
+    nat.css("section.current-exhibitions article.mod").collect do |exhibit|
+      exhibit_info = {
+        :title => exhibit.css("h1 a").text,
+        :closing_date => exhibit.css("p.date").text,
+        :exhibit_link => "http://www.amnh.org#{exhibit.css("h1 a").attribute("href").value}"
+      }
+
+      exhibit_page = Nokogiri::HTML(open(exhibit_info[:exhibit_link]))
+      exhibit_info[:about_exhibit] = exhibit_page.css("div.attribute-body p").text
+
+      exhibit_info
     end
-
+  end
 end
